af22d6fbfff0afda9979f0593cae454bcd76d74f
Wm. Butler Bushyhead <wbbushyhead@gmail.com>
2014-07-29 13:42:06 -0700

    Commented out original stack code. Implemented a queue stack. Changed 2 method names and functionality. Added a few simple print tests.

--- stack.rb
+++ stack.rb
@@ -1,20 +1,49 @@
+#Original Stack Class
+
+# class Stack
+
+#   def initialize
+#     @store = []
+#   end
+
+#   def push(x)
+#     @store.push(x)
+#   end
+
+#   def pop
+#     raise "Alert - Stack is Empty" if self.empty?
+#     @store.pop
+#   end
+
+#   def peek
+#     @store.last
+#   end
+
+#   def empty?
+#     @store.empty?
+#   end
+
+# end
+
+#Queue Stack
+
 class Stack
 
   def initialize
     @store = []
   end
 
-  def push(x)
+  def enqueue(x)
     @store.push(x)
   end
 
-  def pop
+  def dequeue
     raise "Alert - Stack is Empty" if self.empty?
-    @store.pop
+    @store.shift
   end
 
   def peek
-    @store.last
+    @store.first
   end
 
   def empty?
@@ -22,3 +51,12 @@ class Stack
   end
 
 end
+
+new_queue = Stack.new
+
+new_queue.enqueue("a")
+new_queue.enqueue("b")
+new_queue.enqueue("c")
+p new_queue.peek == "a"
+new_queue.dequeue
+p new_queue.peek == "b"
