9e6f508f7450645ce2c58245a28f8de444ac2085
Wm. Butler Bushyhead <wbbushyhead@gmail.com>
2014-07-29 13:25:47 -0700

    Completed Methods.

--- stack.rb
+++ stack.rb
@@ -1,18 +1,24 @@
 class Stack
 
   def initialize
+    @store = []
   end
 
   def push(x)
+    @store.push(x)
   end
 
   def pop
+    raise "Alert - Stack is Empty" if self.empty?
+    @store.pop
   end
 
   def peek
+    @store.last
   end
 
   def empty?
+    @store.empty?
   end
 
 end
