a616755e229c04c16738ec3ee0dc77b9e884f775
Wm. Butler Bushyhead <wbbushyhead@gmail.com>
2014-08-07 15:14:28 -0700

    Added methods for linked list class

--- linked_list.rb
+++ linked_list.rb
@@ -1,13 +1,34 @@
 class Node
+  attr_accessor :content, :next_node
 
-  def initialize
+  def initialize(content, next_node = nil)
+    @content = content
+    @next_node = next_node
   end
 
 end
 
 class List
+  attr_accessor :head
 
-  def initialize
+  def initialize(head)
+    @head = head
+  end
+
+  def add_node
+
+  end
+
+  def remove_node
+
+  end
+
+  def get
+
+  end
+
+  def size
+
   end
 
 end
