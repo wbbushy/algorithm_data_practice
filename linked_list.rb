7e86bd7dd6f4b3c4301abd45c18213d4275a0632
Wm. Butler Bushyhead <wbbushyhead@gmail.com>
2014-08-08 11:58:10 -0700

    Added code for the methods added, changed get to display, added test code

--- linked_list.rb
+++ linked_list.rb
@@ -15,20 +15,56 @@ class List
     @head = head
   end
 
-  def add_node
-
+  def add_node(value)
+    current_node = @head
+    while current_node.next_node != nil
+      current_node = current_node.next_node
+    end
+    current_node.next_node = Node.new(value,nil)
   end
 
-  def remove_node
-
+  def remove_node(value)
+    current_node = @head
+    if current_node.content == value
+      @head = current_node.next_node
+    else
+      while current_node != nil && current_node.next != nil && current_node.next_node.content != nil
+        current_node = current_node.next
+      end
+    end
+    if current_node != nil && current_node.next_node != nil
+      current_node.next_node =current_node.next_node.next_node
+    end
   end
 
-  def get
-
+  def display
+    current_node = @head
+    while current_node.next_node != nil
+      p current_node.content
+      current_node = current_node.next_node
+    end
+      p current_node.content
   end
 
   def size
-
+    current_node = @head
+    length = 1
+    if current_node.content == nil
+      return length - 1
+    else
+      while current_node.next_node != nil
+        length +=1
+        current_node = current_node.next_node
+      end
+      return length
+    end
   end
 
 end
+
+
+list = List.new(Node.new(1))
+list.add_node(2)
+list.add_node(3)
+list.display
+
