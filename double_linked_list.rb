79a9b69c41128871f60d7da88d594c2bffd02e59
Wm. Butler Bushyhead <wbbushyhead@gmail.com>
2014-08-14 14:08:18 -0700

    Added more test code and fixed a formatting error

--- double_linked_list.rb
+++ double_linked_list.rb
@@ -15,4 +15,57 @@ class DoubleLinkedList
   def initialize(head)
     @head = head
   end
+
+  def add_node(value)
+    current_node = @head
+    while current_node.next_node != nil
+      current_node = current_node.next_node
+    end
+    current_node.next_node = Node.new(value,nil)
+  end
+
+  def remove_node(value)
+    current_node = @head
+    if current_node.content == value
+      @head = current_node.next_node
+    else
+      while current_node != nil && current_node.next_node != nil && current_node.next_node.content != value
+        current_node = current_node.next_node
+      end
+    end
+    if current_node != nil && current_node.next_node != nil
+      current_node.next_node = current_node.next_node.next_node
+    end
+  end
+
+  def display
+    current_node = @head
+    while current_node.next_node != nil
+      p current_node.content
+      current_node = current_node.next_node
+    end
+      p current_node.content
+  end
+
+  def size
+    current_node = @head
+    length = 1
+    if current_node.content == nil
+      return length - 1
+    else
+      while current_node.next_node != nil
+        length +=1
+        current_node = current_node.next_node
+      end
+      return length
+    end
+  end
+
+  def add_after_value(value)
+
+  end
+
+  def add_before_value(value)
+
+  end
 end
