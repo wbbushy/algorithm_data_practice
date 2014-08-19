b404d13be81418dfbed5d627274320fe4062d022
Wm. Butler Bushyhead <wbbushyhead@gmail.com>
2014-08-19 00:39:32 -0700

    Added attribute accessors. Added add after value method. Added test code to ensure expected functionality.

--- circular_linked_list.rb
+++ circular_linked_list.rb
@@ -1,4 +1,5 @@
 class Node
+  attr_accessor :content, :next_node, :prev_node
 
   def initialize(content, next_node = nil)
     @content = content
@@ -8,10 +9,21 @@ class Node
 end
 
 class CircularLinkedList
+  attr_accessor :head
 
   def initialize(head, tail = nil)
     @head = head
     @tail = tail
+  end
+
+  def add_after_value(value, content)
+    current_node = @head
+    until current_node.content == value
+      current_node = current_node.next_node
+    end
+    new_node = Node.new(content)
+    new_node.next_node = current_node.next_node
+    current_node.next_node = new_node
   end
 
   def remove_node(value)
@@ -29,3 +41,11 @@ class CircularLinkedList
   end
 
 end
+
+node_1 = Node.new(1)
+list = CircularLinkedList.new(node_1)
+list.add_after_value(1, 2)
+list.add_after_value(2, 3)
+p list.head.content == 1
+p list.head.next_node.content == 2
+p list.head.next_node.next_node.content == 3
