d7d1a2d34af2deba1e09b0e4aab3f88f42401291
Wm. Butler Bushyhead <wbbushyhead@gmail.com>
2014-08-22 14:40:32 -0700

    First stab at binary tree. Created Node and BinaryTree classes both with initialize methods. Node is initialized with a data instance variable, and left and right instance variables that starts at nil, and Binary Tree starts with an initialize method that has an empty array as an instance variable

---
+++ binary_tree.rb
@@ -0,0 +1,17 @@
+class Node
+
+  def initialzie(data, left = nil, right = nil)
+    @data = data
+    @left = left
+    @right = right
+  end
+
+end
+
+class BinaryTree
+
+  def initialize
+    @tree = []
+  end
+
+end
